0eede12c1f2f0b2fa66cfafa2091c69f00f2ecc9
Huy Dang <ghuydang@gmail.com>
2024-03-20 15:20:50 -0700

    Add deploy endpoint with repository cloning

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import cors from "cors";
 import simpleGit from "simple-git";
+import { generate } from "./utils";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.post("/deploy", async (req, res) => {
+  const repoUrl = req.body.repoUrl;
+  const id = generate();
+  await simpleGit().clone(repoUrl, `output/${id}`);
+
+  res.json({
+    id: id,
+  });
+});
 
 app.listen(3000);
