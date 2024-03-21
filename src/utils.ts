77570c33b6716fa11db64236afbac21cd8e81c56
Huy Dang <ghuydang@gmail.com>
2024-03-21 10:54:38 -0700

    Write function to get the path of all files of a directory

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+
 export function generate(len = 5) {
   const subset = "123456789qwertyuiopasdfghjklzxcvbnm";
   let id = "";
@@ -5,4 +8,21 @@ export function generate(len = 5) {
     id += subset[Math.floor(Math.random() * subset.length)];
   }
   return id;
+}
+
+export function getAllFiles(folderPath: string) {
+  let pathsList: string[] = [];
+
+  const allFilesAndFolders = fs.readdirSync(folderPath);
+  allFilesAndFolders.forEach((file) => {
+    const fullFilePath = path.join(folderPath, file);
+
+    if (fs.statSync(fullFilePath).isDirectory()) {
+      pathsList = pathsList.concat(getAllFiles(fullFilePath));
+    } else {
+      pathsList.push(fullFilePath);
+    }
+  });
+
+  return pathsList;
 }
