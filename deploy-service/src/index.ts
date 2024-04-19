f62e7a324d67a2c98086424eea20025841dec12c
Huy Dang <ghuydang@gmail.com>
2024-04-18 20:55:51 -0700

    Write a listener for the main queue

---
+++ deploy-service/src/index.ts
@@ -0,0 +1,17 @@
+import { commandOptions, createClient } from "redis";
+
+const subscriber = createClient();
+subscriber.connect();
+
+async function main() {
+  while (1) {
+    const response = await subscriber.brPop(
+      commandOptions({ isolated: true }),
+      "build-queue",
+      0,
+    );
+    console.log(response);
+  }
+}
+
+main();
